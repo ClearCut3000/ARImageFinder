0bd578d1d06f7016fbb979afaa67c97a06b61183
Nikolay Nikitin <nikitin.nikolay.v@gmail.com>
2021-09-29 00:17:54 +0300

    Added fading animation

--- ARImageFinder/ViewController.swift
+++ ARImageFinder/ViewController.swift
@@ -5,13 +5,16 @@
 //  Created by Николай Никитин on 26.09.2021.
 //
 
-import UIKit
-import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
   @IBOutlet var sceneView: ARSCNView!
+
+  let videoPlayer: AVPlayer = {
+    let url = Bundle.main.url(forResource: "usd", withExtension: "mp4", subdirectory: "art.scnassets")!
+    return AVPlayer(url: url)
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -30,10 +33,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     // Create a session configuration
     let configuration = ARImageTrackingConfiguration()
     // Detect image.
-    configuration.maximumNumberOfTrackedImages = 2
+    configuration.maximumNumberOfTrackedImages = 5
     configuration.trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil)!
 
-//    configuration.planeDetection = .horizontal
     // Run the view's session
     sceneView.session.run(configuration)
   }
@@ -70,7 +72,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     156 / 150 / 8.525 * size.width :
     156 / 150 / 8.7668 * size.width
     let plane = SCNPlane(width: width, height: height)
-    plane.firstMaterial?.diffuse.contents = image.name == "horses" ? UIImage(named: "ben") : UIImage(named: "capitol")
+    plane.firstMaterial?.diffuse.contents = image.name == "horses" ? UIImage(named: "ben") : videoPlayer
+
+    if image.name == "horses" {
+      videoPlayer.play()
+    }
 
     // Create plane node
     let planeNode = SCNNode(geometry: plane)
@@ -79,6 +85,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     // Move plane node
      planeNode.position.x += image.name == "theatre" ? 0.01 : 0
 
+    // Run animation
+    planeNode.runAction(.sequence([
+      .wait(duration: 10),
+      .fadeOut(duration: 3),
+      .removeFromParentNode()
+    ]))
+
+    // Add child node
     node.addChildNode(planeNode)
   }
   func nodeAdded(_ node: SCNNode, for planeAnchor: ARPlaneAnchor){
